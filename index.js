6c9f75f44cb2572bdb45351ccd7557947eb69cbc
nishthaq3 <nishthatripathi05@gmail.com>
2026-02-04 14:27:20 +0530

    Search button added, functional

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+let btn=document.getElementById("searchBtn")
 let allProducts=[]
 //fetching api into a global array
 async function getAPI(){
@@ -5,7 +6,7 @@ async function getAPI(){
 	let data= await res.json()
 	allProducts=data
 
-	showProducts()
+	showProducts(allProducts)
 }
 getAPI()
 
@@ -13,16 +14,32 @@ getAPI()
 //create div
 //add text to div
 //append it to body
-function showProducts(){
-	allProducts.forEach(p=>{
+function showProducts(products){
+	let container=document.getElementById("products")
+	container.innerHTML=""
+
+	products.forEach(p=>{
 		let card=document.createElement("div")
 		card.className="card";
 		//we made a class for them so that styling is easy
 		card.innerHTML=`
 		<img src="${p.image}">
 		<h5>${p.title}</h5>
-		<h5>Price: ₹${p.price}</h5>
+		<p>Price: ₹${p.price}</p>
 		`
-		document.getElementById("products").appendChild(card)
+		container.appendChild(card)
 	})
 }
+//now for the search button to work, we have to regisger it as an event
+btn.addEventListener("click",()=>{
+	//first store whatever value is beign typed into the button
+	let text=document.getElementById("search").value
+	//get the value beign typed in searchbox
+	//now wat to do with that value?
+	//filter it from the list of products that we have
+	let filtered=allProducts.filter(p=>
+		p.title.toLowerCase().includes(text.toLowerCase()))
+		//searches the title after converting it to lower case and check if what user 
+		//is typing is in the title
+		showProducts(filtered)
+})
